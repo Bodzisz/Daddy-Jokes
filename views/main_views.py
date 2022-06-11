d5340780c03933c1fc37e5de6bf909bf2e3aaefe
Bodzisz <kacpiw01pol@gmail.com>
2022-06-11 19:39:22 +0200

    Setup landing page and navbar

--- views/main_views.py
+++ views/main_views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect
 from flask_login import login_required, current_user
 from db.mongo_db import mongo
-import datetime
+from random import randrange
 
 main = Blueprint('view', __name__, template_folder='templates',
                  static_folder='static')
@@ -13,7 +13,13 @@ jokes_collection = mongo.jokes
 def home():
     username = current_user.username if current_user.is_authenticated else None
     is_authenticated = current_user.is_authenticated
-    return render_template('index.html', username=username, is_authenticated=is_authenticated)
+    random_joke = get_random_joke()
+    return render_template('index.html', username=username, is_authenticated=is_authenticated, random_joke=random_joke)
+
+
+def get_random_joke():
+    rand = randrange(jokes_collection.count_documents({}))
+    return jokes_collection.find()[rand]
 
 
 @main.route("/dashboard")
